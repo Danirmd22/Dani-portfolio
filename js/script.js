2e294854681908caee115c27b60e3c9600d9e8be
Danirmd22 <117454588+Danirmd22@users.noreply.github.com>
2024-06-10 09:42:56 +0200

    chore: Update social icons styling, layout, and navigation links to Spanish language

--- js/script.js
+++ js/script.js
@@ -18,3 +18,22 @@ var app = function () {
     };
     init();
 }();
+
+document.addEventListener('DOMContentLoaded', function () {
+    // typing animation
+    function writeText(content, elem) {
+      var contentArray = content.split("");
+      var current = 0;
+      var intervalId = setInterval(function () {
+        if (current < contentArray.length) {
+          elem.textContent += contentArray[current++];
+        } else {
+          clearInterval(intervalId);
+        }
+      }, 80);
+    }
+  
+    // input text for typing animation
+    var holder = document.querySelector('#two-title');
+    writeText("FRONT-END DEVELOPER", holder);
+  });
